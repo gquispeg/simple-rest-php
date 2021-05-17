434c99667434e66363593a60bc208281f0700d53
gian2051 <giangbd5@hotmail.com>
2021-05-17 16:20:10 -0500

    nuevas funciones

--- index.php
+++ index.php
@@ -1,42 +1,26 @@
 <?php
-//header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS');
+$srv = new server;
+$rsl['metodo']=$srv->getMethod();
 
-$rsl['metodo'] = $_SERVER['REQUEST_METHOD'];
-$rsl['url'] =  $_SERVER['REQUEST_URI'];
-
-//obtener solo la cadena sin página web
-$divi	=$_SERVER['REQUEST_URI'];
-echo substr($divi, strpos($divi,'/',1)+1);
-
-
-$method = $_SERVER['REQUEST_METHOD'];
-switch($method){
-    case 'PUT':
-        $this->create_contact($name);
-        break;
-    case 'DELETE':
-        $this->delete_contact($name);
-        break;
-    case 'GET':
-        $srv = new servicio;
-		$srv->setStatus(200);
-		$srv->setHeader(array('Content-Type: application/json'));
-		$srv->setBody(json_encode($rsl));
-		$srv->run();
-        break;
-    default:
-        header('HTTP/1.1 405 Method not allowed');
-        header('Allow: GET, PUT, DELETE');
-        break;
-}
+$srv->setStatus(200);
+$srv->setHeader(array('Content-Type: application/json'));
+$srv->setBody(json_encode($rsl));
+$srv->run();
 
 
 
-class servicio{
+
+
+
+class server{
 	private $status	= 200;
 	private $header	= array('Content-Type');
 	private $body	= '';	
-	
+
+	//Propiedades SET
+	//header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS');
+	//Crear function para Metodos permitidos
+
 	public function setStatus($codigo){
 		$this->status = $codigo;
 	}
@@ -48,24 +32,46 @@ class servicio{
 	public function setHeader($header){
 		$this->header = $header;
 	}
+
+	//Propiedades GET
+	public function getMethod(){
+		return $_SERVER['REQUEST_METHOD'];
+	}
+
+	public function getPage(){
+		return $_SERVER['REQUEST_URI'];
+	}
+
+	public function getHeader(){
+		//obtener solo la cadena sin página web
+		$divi	=$_SERVER['REQUEST_URI'];
+		return substr($divi, strpos($divi,'/',1)+1);
+	}
 	
+	public function getBody(){
+		//leer el body enviado en formato raw-json
+		return file_get_contents('php://input');
+	}
+
+	public function getParametros(){
+		//enviado como parametro en la barra de direcciones despues del ?
+		return json_encode($_REQUEST);
+	}
+	
+	public function getAuthBasica(){
+		//autprization basica
+		$rsl['user']		=$_SERVER['PHP_AUTH_USER'];
+		$rsl['password']	$_SERVER['PHP_AUTH_PW'];
+		
+		return $rsl;
+	}
+
+	//Ejecutar
 	public function run(){
 		foreach($this->header as $cadena){
 			header($cadena);
 		}
-			
-		/*
-		--------enviado como parametro en la barra de direcciones despues del ?
-		echo json_encode($_REQUEST);
 		
-		
-		--------autprization basica
-		echo "<p>Hola {$_SERVER['PHP_AUTH_USER']}.</p>";
-		echo "<p>Introdujo {$_SERVER['PHP_AUTH_PW']} como su contraseña.</p>";
-		
-		--------leer el body enviado en formato raw-json
-		echo file_get_contents('php://input');
-		*/
 		http_response_code($this->status);
 		echo $this->body;
 	}
